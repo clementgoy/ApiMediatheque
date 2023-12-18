5a77d88426e3a1563bb35b5a5e014761fb3893cd
SimonTvr <stauvron@ensc.fr>
2023-12-18 16:38:53 +0100

    FUNC_02
    FUNC_02 fonctionnelle mais cela met des messages d'erreur sur le swagger pour le POST et le DELETE

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -36,4 +36,58 @@ return NotFound();
 return document;
 }
 
+// POST: api/document
+[HttpPost]
+public async Task<ActionResult<Document>> PostDocument(Document document)
+{
+_context.Documents.Add(document);
+await _context.SaveChangesAsync();
+
+
+return CreatedAtAction(nameof(GetDocument), new { id = document.Id }, document);
+}
+
+// PUT: api/doucment/2
+[HttpPut("{id}")]
+public async Task<IActionResult> PutDocument(int id, Document document)
+{
+if (id != document.Id)
+return BadRequest();
+
+
+_context.Entry(document).State = EntityState.Modified;
+
+
+try
+{
+await _context.SaveChangesAsync();
+}
+catch (DbUpdateConcurrencyException)
+{
+if (!_context.Documents.Any(m => m.Id == id))
+return NotFound();
+else
+throw;
+}
+
+return NoContent();
+}
+
+// DELETE: api/document/2
+[HttpDelete("{id}")]
+public async Task<IActionResult> DeleteDocument(int id)
+{
+var document = await _context.Documents.FindAsync(id);
+
+
+if (document == null)
+return NotFound();
+
+
+_context.Documents.Remove(document);
+await _context.SaveChangesAsync();
+
+return NoContent();
+}
+
 }
