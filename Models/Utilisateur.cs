f351efc94ef7eaf454158255e1a1e8f22ca045e0
clementgoy <118727699+clementgoy@users.noreply.github.com>
2024-01-05 21:13:00 +0100

    correction du commit précédent

--- Models/Utilisateur.cs
+++ Models/Utilisateur.cs
@@ -4,22 +4,14 @@ public class Utilisateur
     public string Nom { get; set; }
     public string Prenom { get; set; }
     public string Email { get; set; }
-    
-    public class Utilisateur
+
+    public Utilisateur() { }
+
+    public Utilisateur(UtilisateurDTO utilisateurDTO)
     {
-        public int Id { get; set; }
-        public string Nom { get; set; }
-        public string Prenom { get; set; }
-        public string Email { get; set; }
-
-        public Utilisateur() { }
-
-        public Utilisateur(UtilisateurDTO utilisateurDTO)
-        {
-            Id = utilisateurDTO.Id;
-            Nom = utilisateurDTO.Nom;
-            Prenom = utilisateurDTO.Prenom;
-            Email = utilisateurDTO.Email;
-        }
+        Id = utilisateurDTO.Id;
+        Nom = utilisateurDTO.Nom;
+        Prenom = utilisateurDTO.Prenom;
+        Email = utilisateurDTO.Email;
     }
 }
