a9ee2fa6731e587946f3214a2cafb18b6454bb25
clementgoy <118727699+clementgoy@users.noreply.github.com>
2024-01-06 17:49:37 +0100

    correction rapide

---
+++ Migrations/20240106164650_NomDeLaMigration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTodo.Migrations
+{
+    public partial class NomDeLaMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateEmprunt",
+                table: "Emprunts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateEmprunt",
+                table: "Emprunts");
+        }
+    }
+}
